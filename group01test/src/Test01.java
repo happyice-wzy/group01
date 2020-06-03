69b084d5fc3663bb973a51e8e0bae10cf02e6db2
lmttting <linmt.japan@gmail.com>
2020-06-03 17:47:25 +0900

    lmt测试

--- group01test/src/Test01.java
+++ group01test/src/Test01.java
@@ -2,4 +2,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("helloworld");
     }
+
+    public static void lmt(){
+        System.out.println("lmt测试...");
+    }
 }
