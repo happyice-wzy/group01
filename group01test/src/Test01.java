b39576ec955b2f57235459832b3632f5a159dffa
180207ij@yse-c.net <Lm03507991>
2020-06-03 18:22:44 +0900

    lm创建的方法

--- group01test/src/Test01.java
+++ group01test/src/Test01.java
@@ -2,4 +2,7 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("helloworld");
     }
+    public static void lm(String[] args) {
+        System.out.println("helloworld");
+    }
 }
