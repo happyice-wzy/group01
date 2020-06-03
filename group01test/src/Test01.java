9729ceeda4f321a0431b92268f38d3fb535539fb
hutf-51100726 <hutf_51100726@163.com>
2020-06-03 16:58:18 +0900

    hutf创建的方法

--- group01test/src/Test01.java
+++ group01test/src/Test01.java
@@ -2,4 +2,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("helloworld");
     }
+
+    public void hutf(){
+        System.out.println("hutf 测试.........");
+    }
 }
